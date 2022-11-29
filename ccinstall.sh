b3c21d52e60313dce310c75d7e09f5c0e4f39aae
HSGamer <huynhqtienvtag@gmail.com>
2022-11-29 11:54:35 +0700

    Description on install scripts

--- ccinstall.sh
+++ ccinstall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "> Clone EduBlock chaincode"
 git clone https://github.com/etases/EduBlock.CC.git ./vars/chaincode/edublockcc/java
 
+echo "> Install EduBlock chaincode"
 ./minifab ccup -n edublockcc -l java -v 1.0 -r true -p '"init"'
+
+echo "> Completed"
+echo "- The EduBlock chaincode is installed on the current channel of your network."
+echo "- Use the chaincode name \"edublockcc\" to invoke the chaincode from your application."
--- networkup.sh
+++ networkup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo "> Install Minifabric"
+curl -o minifab -sL https://tinyurl.com/yxa2q6yr && chmod +x minifab
+
+echo "> Start Minifabric"
 ./minifab up -s couchdb -i 2.4 -c mychannel -l java -e true
 
+echo "> Grant file permissions"
 sudo chmod -R 777 ./vars
+
+echo "> Completed"
+echo "- You can run the \"minifab\" command to manage your network."
+echo "- You can run the \"ccinstall.sh\" script to install the chaincode from EduBlock."
